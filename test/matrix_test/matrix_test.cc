4c877a3eb12e762a7e88f0deae56037157dccf97
WU,Qihang <wu.qihang@hotmail.com>
2019-09-11 13:48:07 -0400

    fixed a bug in matrix << operator

--- test/matrix_test/matrix_test.cc
+++ test/matrix_test/matrix_test.cc
@@ -7,22 +7,29 @@ int main() {
     // unsigned int ja[] = {0, 1, 2, 0, 1, 0, 2};
     // double a[] = {6.0, 8.0, 8.0, 2.0, 9.0, 5.0, 5.0};
 
-    std::size_t ia[] = {0, 3, 5, 6};
-    std::size_t ja[] = {0, 1, 3, 0, 2, 1};
-    double a[] = {1, 2, 4, 3, 5, 1};
+    /* std::size_t ia[] = {0, 3, 5, 6}; */
+    // std::size_t ja[] = {0, 1, 3, 0, 2, 1};
+    // double a[] = {1, 2, 4, 3, 5, 1};
+/*  */
+
+    std::size_t ia[] = {0, 4, 7, 9, 10, 13};
+    std::size_t ja[] = {0, 1, 2, 4, 0, 1, 3, 0, 2, 3, 0, 1, 4};
+    double a[] = {1.0, 2.0, 3.0, 5.0, 2.0, 10.0, 1.0, 3.0, 6.0, 5.0, 5.0, 1.0, 4.0};
 
     std::size_t N = sizeof(ia)/sizeof(std::size_t) - 1;
     std::size_t NNZ = sizeof(a)/sizeof(double);
 
     std::cout << N << std::endl;
 
-    iaja::SparseMatrix<double> A(N, 4, NNZ, ia, ja, a);
+    iaja::SparseMatrix<double> A(N, 5, NNZ, ia, ja, a);
     std::cout << "Original Matrix: \n";
     std::cout << A << std::endl;
 
     iaja::SparseMatrix<double> B = A.transpose();
     std::cout << "Transposed Matrix: \n";
     std::cout << B << std::endl;
+
+
 
     return EXIT_SUCCESS;
 }
