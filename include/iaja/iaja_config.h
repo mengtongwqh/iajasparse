1bc6dafe59281b39cdbed974c1543bd245cdb7a4
WU,Qihang <wu.qihang@hotmail.com>
2019-09-16 11:38:43 -0400

    added profiling flag for timing

--- include/iaja/iaja_config.h
+++ include/iaja/iaja_config.h
@@ -5,6 +5,8 @@
 const unsigned int PRINT_WIDTH_DOUBLE = 15;
 const unsigned int PRINT_WIDTH_UNSIGNED_INT = 5;
 
+#define PROFILING
+
 // macros to start and end iaja namespace
 #define IAJA_NAMESPACE_OPEN namespace iaja {
 #define IAJA_NAMESPACE_CLOSE }
