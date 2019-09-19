a1f1b006faa8de9c0c46adfd1e3820af03fb11bb
WU,Qihang <wu.qihang@hotmail.com>
2019-09-19 17:30:55 -0400

    added SizeType which is a typedef for std::size_t

--- include/iaja/iaja_config.h
+++ include/iaja/iaja_config.h
@@ -1,9 +1,11 @@
 #ifndef _IAJA_CONFIG_H_
 #define _IAJA_CONFIG_H_
+#include <cstddef>
 
 // parameters controlling print styles
 const unsigned int PRINT_WIDTH_DOUBLE = 15;
 const unsigned int PRINT_WIDTH_UNSIGNED_INT = 5;
+using SizeType = std::size_t;
 
 #define PROFILING
 
