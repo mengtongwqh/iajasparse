67a5605e55388f633956042074965fed9af8ff97
WU,Qihang <wu.qihang@hotmail.com>
2019-09-19 17:32:15 -0400

    added output for sparsity pattern and LoF

--- test/img_matrix/andre_std_test.cc
+++ test/img_matrix/andre_std_test.cc
@@ -1,10 +1,11 @@
 #include <cmath>
+#include <fstream>
 #include "img_matrix.h"
 
 using iaja::ImgMatrixOrthomin;
 using iaja::ImgMatrixPCG;
 
-const double FLOATING_POINT_TOLERANCE = 1e-5;
+const double FLOATING_POINT_TOLERANCE = 1e-3;
 
 int main(int argc, char* argv[]) {
 
@@ -17,16 +18,16 @@ int main(int argc, char* argv[]) {
     }
 
     std::size_t ngrid = atoi(argv[1]);
-    unsigned int max_iter = 100;
-    double tol = 1e-10;
-    unsigned int max_level_of_fill = 1;
+    unsigned int max_iter = 10000;
+    double tol = 1e-3;
+    unsigned int max_level_of_fill = 3;
     unsigned int NORTH = 5;
     std::string reorder_method = "natural";
 
     /*** CONSTRUCT TEST OBJECT ***/
 
-    ImgMatrixOrthomin testobj(ngrid, NORTH, max_iter, tol);
-    // ImgMatrixPCG testobj(ngrid, max_iter, tol);
+    // ImgMatrixOrthomin testobj(ngrid, NORTH, max_iter, tol);
+    ImgMatrixPCG testobj(ngrid, max_iter, tol);
 
     testobj.img_rhs();
     testobj.img_lhs();
@@ -36,12 +37,24 @@ int main(int argc, char* argv[]) {
     start = clock();
     testobj.solver.symbolic_factor(reorder_method, max_level_of_fill);
     after_ilu = clock();
-    std::cout << "ILU time " << std::fixed << double(after_ilu-start)/double(CLOCKS_PER_SEC) << std::setprecision(5) << std::endl;
+    std::cout << "ILU symbolic factorization time " << std::scientific << double(after_ilu-start)/double(CLOCKS_PER_SEC) << std::setprecision(5) << std::endl;
+    
+    std::ofstream spfile("sparsity_pattern.txt", std::ofstream::out);
+    std::ofstream loffile("level_of_fill.txt", std::ofstream::out);
+    testobj.solver.ilu.print_level_of_fill(loffile);
+    testobj.lhs.print_compressed(spfile);
+    spfile.close();
+    loffile.close();
+    return EXIT_SUCCESS;
 
     if (testobj.solver.iterative_solve(testobj.rhs, testobj.x) == EXIT_SUCCESS)
         std::cout << std::scientific << "iterative solver completed with residual norm " << testobj.solver.get_residual_norm()
             << " after iteration count " << testobj.solver.get_iter_count() << std::endl;
+    // std::cout << testobj.solver.ilu  << std::endl;
     after_iter = clock();
+
+    // std::cout << testobj.lhs << std::endl;
+
 
     for (std::size_t i = 0; i < testobj.x.length(); ++i) {
         if ( fabs(testobj.x[i] - i - 1) > FLOATING_POINT_TOLERANCE ) {
@@ -51,7 +64,7 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "Andre's standard M-matrix test passed\n";
 
-    std::cout << "Iter solve time " << std::fixed << double(after_iter-after_ilu)/double(CLOCKS_PER_SEC) << std::setprecision(5) << std::endl;
+    std::cout << "Iter solve time " << std::scientific << double(after_iter-after_ilu)/double(CLOCKS_PER_SEC) << std::setprecision(5) << std::endl;
     std::cout << "TOTAL TIME: " << std::fixed << double(after_iter-start)/double(CLOCKS_PER_SEC) << std::setprecision(5) << std::endl;
 
     return EXIT_SUCCESS;
